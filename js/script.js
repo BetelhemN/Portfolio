625ab9dbe364600940c9eb492a65e485ff7e03bb
Betelhem Negash <betelhemnegash10@gmail.com>
2023-03-08 22:54:28 -0800

    My First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*  ================== Typing Animation ====================  */
+var typed = new typed(".typing", {
+    strings:["Softawre Engineer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
